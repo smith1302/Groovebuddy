e3345d64feb1799316ca418b35698724e8d057c9
Eric Smith <ericsmith1302@gmail.com>
2013-07-03 20:57:05 -0700

    Hiding db stuff

--- dbconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try {
-		$conn = new PDO('mysql:host=127.0.0.1;dbname=groovebuddy', 'root', 'lolly', array(PDO::ATTR_PERSISTENT => true));
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}catch(PDOException $e) {
-		echo "Error".$e->getMessage();
-	}
-?>
